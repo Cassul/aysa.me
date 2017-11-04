087066e17d6b8efa29d44c531f884c959f58467c
Cassul <aysa.garden@gmail.com>
2017-11-05 10:24:53 +1100

    dead end

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 //store express application in this variable
-const reload = require('reload');
-//to reload the page after updating
 const bodyParser = require('body-parser');
 //to get data from users
 const cookieParser = require('cookie-parser');
@@ -10,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 //to install mongoose - ODM(object data modeling)
 const path = require('path');
+const net = require('net');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
@@ -21,18 +20,28 @@ app.set('view engine', 'pug');
 var routes = require('./routes/index');
 app.use('/', routes);
 //set routes
+const server = net.createServer((socket) => {
+  socket.end('goodbye\n');
+}).on('error', (err) => {
+  // handle errors here
+  throw err;
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 //sets what sources it can use in what folder
 //app.use(require('./jason'));
 
-var server = app.listen(app.get('port'), function() {
-  console.log('listening to port ' + app.get('port'));
+/*server.listen(app.get('port'), function() {
+  console.dir('listening to port ' + app.get('port'));
 });
-//it's done only to log on shell what port it listens toç
+//it's done only to log on shell what port it listens to*/
+server.listen(3000, () => {
+  console.dir('opened server on', server.address());
+});
 
 //I'm using mongolab addon for heroku, I had to find MONGOLAB_URI through shell to connect my application to mongodb
-if (server.address().port == 3000) {
+/*if (server.address().port == 3000) {
 mongoose.connect('mongodb://localhost/test_project', {
 	useMongoClient: true,
 });}
@@ -40,33 +49,34 @@ else {var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://heroku_ggck1lhb:t96sat2q3aufmpc7sh8th2r600@ds237815.mlab.com:37815/heroku_ggck1lhb';
-    //That is MONGOLAB_URI
+    //That is MONGOLAB_URI*/
 
     // The http server will listen to an appropriate port, or default to
     // port 5000.
-    var theport = process.env.PORT || 5000;
+    //var theport = process.env.PORT || 5000;
 
     // Makes connection asynchronously.  Mongoose will queue up database
     // operations and release them when the connection is complete.
-    mongoose.connect(uristring, function (err, res) {
+    /*mongoose.connect(uristring, function (err, res) {
       if (err) {
       console.log ('ERROR connecting to: ' + uristring + '. ' + err);
       } else {
       console.log ('Succeeded connected to: ' + uristring);
       }
     });
-}
+*/
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 //to work with mongo data go to mlab.com
 
 // Check if we are running on localhost. If not then implement fix for Heroku
-if (server.address().port == 3000) {
+/*if (server.address().port == 3000) {
   app.set('views','./views');
 }
 else {
   app.set('views', './views');
-}
+}*/
 //sets template engine to pug
 //by default templates are located in a folder views
 
@@ -86,9 +96,3 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
-
-
-
-reload(app);
