45d86f0222665e797c4df639da462626a694a00b
Cassul <aysa.garden@gmail.com>
2017-12-28 21:16:56 +1000

    GFS

--- public/facebook.js
+++ public/facebook.js
@@ -5,15 +5,4 @@
   js.src = 'https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.11';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-console.log('hey');
-
-window.onload = function () {
-  let fb = document.getElementById('fb');
-  fb.setAttribute('data-href', document.URL);
-  let facebook = document.getElementById('facebook');
-  let route = document.URL.slice(7);
-  route = encodeURIComponent(route);
-  let address = 'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F' + route + '&amp;src=sdkpreparse';
-  facebook.setAttribute('plugin ', address);
-};
 
